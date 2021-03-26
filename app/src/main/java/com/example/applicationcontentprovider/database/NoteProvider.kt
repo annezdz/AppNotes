0f1838837299420109f290f9d2211e78872b94f2
annezdz <annezdz@hotmail.com>
2021-03-25 23:36:56 -0300

    Changed NoteProvider

--- app/src/main/java/com/example/applicationcontentprovider/database/NoteProvider.kt
+++ app/src/main/java/com/example/applicationcontentprovider/database/NoteProvider.kt
@@ -2,9 +2,15 @@ package com.example.applicationcontentprovider.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
 import android.net.Uri
+import android.provider.BaseColumns._ID
+import android.provider.ContactsContract.Intents.Insert.NOTES
+import com.example.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TABLE_NOTES
 
 class NoteProvider : ContentProvider() {
 
@@ -29,7 +35,11 @@ class NoteProvider : ContentProvider() {
         mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
         mUriMatcher.addURI(AUTHORITY,"notes", NOTES)
         mUriMatcher.addURI(AUTHORITY, "notes/#",NOTES_BY_ID)
-        if(context != null){
+        /*Utilizando o Cast (as) para evitar o double bang do Kotlin  pois se é
+        * feita a verificação para ver se o context não é nulo, então ele realmente não é
+        * nulo. Usando o cast ele não fica incomodando*/
+
+        if(context != null){dbHelper = NotesDatabaseHelper(context as Context)
 
         }
         return true
@@ -38,18 +48,49 @@ class NoteProvider : ContentProvider() {
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
+        /* Antes de tudo, precisamos verificar a URL.
+        * Criamos a variável db que vai permitir mexer no DB
+        * dbHelper.writableDatabase permite que está habilitado para que consigamos mexer no SQLLite
+        * a val linesAffect
+        * uri.lastPathSegment será um elemento que será deletado, o elemento desse ID*/
+
+
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.delete(TABLE_NOTES,"$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+
+            /*Agora vamos manusear o Content Provider .
+            * Aqui estamos notificando para o Content Provider tudo o que foi modificado no DB.
+            * Para tudo o que acontece no Content Provider precisamos dar um notifyChange.
+            * No final damos um return das linhas afetadas para posteriormente fazer os tratamentos daquilo
+            * que foi deletado.  */
+
+            context?.contentResolver?.notifyChange(uri,null)
+            return linesAffect
+        }else{
+            throw UnsupportedSchemeException("URL inválida para exclusão")
+        }
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+    /*O etType só serve quando temos requisições de arquivos, como não vamos usar isso nesse exemplo
+    * vamos apenas lançar um throw*/
+
+    override fun getType(uri: Uri): String? = throw UnsupportedSchemeException("Uri não implementada")
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+        if(mUriMatcher.match(uri) == NOTES){
+            /*Aqui Uri.withAppendedPath(BASE_URI,id.toString()) estamos interagindo com o Content Provider*/
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val id = db.insert(TABLE_NOTES,null,values)
+            val insertUri = Uri.withAppendedPath(BASE_URI,id.toString())
+            db.close()
+            //Aqui dizemos que houve registro novo inserido.
+            context?.contentResolver?.notifyChange(uri,null)
+            return insertUri
+        }else{
+            throw UnsupportedSchemeException("URL inválida para inserção.")
+        }
     }
 
 
@@ -57,14 +98,44 @@ class NoteProvider : ContentProvider() {
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+        /*Porque when? porque o select pode autorizar uma pesquisa  tanto por id ou elemento da coluna ou tabela
+        * toda requisição feita no db.query vai para o cursor , que é basicamente o retorno de dados de
+        * um Content Provider */
+        return when {
+            mUriMatcher.match(uri) == NOTES -> {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor =
+                    db.query(TABLE_NOTES,projection,selection,selectionArgs,null,null,sortOrder)
+                cursor.setNotificationUri(context?.contentResolver,uri)
+                cursor
+            }
+            mUriMatcher.match(uri) == NOTES_BY_ID ->{
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor =
+                    db.query(TABLE_NOTES, projection,"$_ID = ?", arrayOf(uri.lastPathSegment),null,null,sortOrder)
+                cursor.setNotificationUri((context as Context).contentResolver,uri)
+                cursor
+            }else ->{
+                throw UnsupportedSchemeException("URL não implementada")
+
+            }
+        }
     }
 
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.update(TABLE_NOTES,values,"$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri,null)
+            return linesAffect
+        }else{
+            throw UnsupportedSchemeException("URL não implementada")
+
+        }
     }
 
     /* Sempre que criarmos uma Content Provider devemos criar uma constraint chamada authority.
